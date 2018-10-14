b061d375cdc9c5663a3a9981f8a432d4a8b5b8b0
Vladanak <vladmyr89@gmail.com>
2018-10-14 19:15:54 +0300

    next

--- server.js
+++ server.js
@@ -1,77 +1,65 @@
-const net = require("net");
-const fs = require("fs");
-const path = require("path");
+const net = require('net');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 const port = 8124;
-let seed = 0;
 
+const reqRemote = 'REMOTE';
+const reqRemCopy = 'COPY';
+const reqRemEncode = 'ENCODE';
+const reqRemDecode = 'DECODE';
+const algorithm = 'aes-128-cbc';
 const reqFiles = 'FILES';
 const reqQA = 'QA';
 const resGood = 'ACK';
 const resBad = 'DEC';
 const resFiles = 'NEXT';
-const defaultDir = process.env.DEF_DIR;
-const maxConn = parseInt(process.env.MAX_CONN);
+const defaultDir = process.env.FILES_DIR;
+const maxConn = parseInt(process.env.CONN);
 
-let files = [];
+let seed = 0;
+let arr = require('./qa.json')
 let clients = [];
+let files = [];
+let fdFile;
 let flag = 0;
 let connections = 0;
 
-let QA;
-let ans;
+
 const server = net.createServer((client) => {
-    client.setEncoding("utf8");
-    client.id = seed++;
-    console.log("Client connected, ID = " + client.id);
     if (++connections === maxConn) {
-        client.write(resBad)
         client.destroy();
     }
-    let log = client.id + ".txt";
-    client.on("data", (data) => {
-        if (data === reqQA || data === reqFiles) {
-            clients[client.id] = data;
-            fs.readFile("qa.json", (err, data) => {
-                if (err) {
-                    console.log("Error read qa.json");
-                    client.destroy();
-                } else {
-                    QA = JSON.parse(data);
-                    fs.appendFile(log, "Connected new client: ID - " + client.id + "\n", (err) => {
-                        if (err) {
-                            console.log("Error append in file");
-                        }
-                    });
-                }
-            });
+    client.setEncoding('utf8');
+
+    client.on('data', (data) => {
+        if (data === reqFiles || data === reqQA || data === reqRemote) {
+            client.id = getUniqID();
             if (data === reqFiles) {
                 files[client.id] = [];
-                fs.mkdir(defaultDir + path.sep + client.id, (err) => {
-                    console.log(err);
-                });
-                fs.appendFile(log, "Making dir for client files\nStarting accept files\n", (err) => {
-                    if (err) {
-                        console.log("Error append in file");
-                    }
-                });
+                fs.mkdir(defaultDir + path.sep + client.id);
             }
-            client.write(resGood);
+            console.log(data);
+            console.log(`Client ${client.id} connected`);
+            clients[client.id] = data;
+            fs.open(`./log/client_${client.id}.txt`, 'w', (err, fd) => {
+                fdFile = fd;
+                client.write(resGood);
+            })
         } else if (client.id === undefined) {
             client.write(resBad);
             client.destroy();
         }
-
         if (clients[client.id] === reqQA && data !== reqQA) {
-            //client.write("DEC");
-            console.log(client.id + " data: " + data);
-            ans = getRandomAnswer();
-            client.write(ans);
-            fs.appendFile(log, "New QA - " + data + "\nServer answer - " + ans + "\n", (err) => {
-                if (err) {
-                    console.log("Error append in file");
-                }
-            });
-            //console.log("Server answer " + ans);
+            console.log(data);
+            let questionInd = getQuestionId(data);
+            let answer;
+            if (answerTheQuestion() === 1) {
+                answer = arr[questionInd].goodAns;
+            } else {
+                answer = arr[questionInd].badAns;
+            }
+            client.write(answer);
         }
         if (clients[client.id] === reqFiles && data !== reqFiles) {
             files[client.id].push(data);
@@ -79,45 +67,57 @@ const server = net.createServer((client) => {
             if (flag === 2) {
                 let buf = Buffer.from(files[client.id][0], 'hex');
                 let filePath = defaultDir + path.sep + client.id + path.sep + files[client.id][1];
-                fs.appendFile(log, "Creating file " + filePath + "\n", (err) => {
-                    if (err) {
-                        console.log("Error append in file");
-                    }
-                });
-                //console.log(filePath);
-                fil = fs.createWriteStream(filePath);
+                let fil = fs.createWriteStream(filePath);
                 fil.write(buf);
-                fs.appendFile(log, "Writing into file " + filePath + "\n", (err) => {
-                    if (err) {
-                        console.log("Error append in file");
-                    }
-                });
                 flag = 0;
                 files[client.id] = [];
-                fil.close();
                 client.write(resFiles);
             }
-            //client.write("Hello from server");
         }
-
-
-    });
-    client.on("end", () => {
-        connections--;
-        console.log("Client disconected, ID = " + client.id);
-        fs.appendFile(log, "client disconected\n", (err) => {
-            if (err) {
-                console.log("Error append in file");
+        if (clients[client.id] === reqRemote && data !== reqRemote) {
+            console.log(data);
+            let remoting = data.split(' ');
+            let rd = fs.createReadStream(remoting[1]);
+            let wr = fs.createWriteStream(remoting[2]);
+            if (remoting[0] === reqRemCopy) {
+                rd.pipe(wr).on('close', sendNext);
             }
-        });
+            if (remoting[0] === reqRemEncode) {
+                let cryptoStream = crypto.createCipher(algorithm, remoting[3]);
+                rd.pipe(cryptoStream).pipe(wr).on('close', sendNext);
+            }
+            if (remoting[0] === reqRemDecode) {
+                let cryptoStream = crypto.createDecipher(algorithm, remoting[3]);
+                rd.pipe(cryptoStream).pipe(wr).on('close', sendNext);
+            }
+        }
+    });
+
+    function sendNext() {
+        client.write(resGood);
+    }
+    client.on('end', () => {
+        connections--;
+        console.log(`Client ${client.id} disconnected`);
     });
 });
 
 server.listen(port, () => {
-    console.log("listenning");
+    console.log(`Server listening on localhost:${port}`);
 });
 
+//*************************************
+function getUniqID() {
+    return Date.now() + seed++;
+}
 
-function getRandomAnswer() {
-    return Math.random() > 0.5 ? '1' : '0';
+function getQuestionId(question) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].question === question)
+            return i;
+    }
+}
+
+function answerTheQuestion() {
+    return Math.random() > 0.5 ? 1 : 0;
 }
